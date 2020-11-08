ef2b44070603c0955307c2859c306f4f70af3f52
vsteinb <vsteinbruegg@uos.de>
2020-11-08 13:25:42 +0100

    show single values on W6

--- ppServer/service/static/service/js/random.js
+++ ppServer/service/static/service/js/random.js
@@ -7,8 +7,8 @@ function roll(num, faces) {
 
 document.addEventListener("DOMContentLoaded", function() {
 
-    [...document.getElementsByClassName("btn")].forEach(tag => tag.addEventListener("click", (e) => {
-        var section = e.currentTarget.parentNode
+    [...document.getElementsByClassName("btn")].forEach(tag => tag.addEventListener("click", ({ currentTarget }) => {
+        var section = currentTarget.parentNode
         const faces = JSON.parse(section.getElementsByClassName("faces")[0].value)
         var num = section.getElementsByClassName("num")[0].value
         num = num === "" ? 0 : parseInt(num)
@@ -17,5 +17,12 @@ document.addEventListener("DOMContentLoaded", function() {
         var sum = 0
         for (var i = 0; i < faces.length; i++) sum += solution[i] * faces[i]
         section.getElementsByClassName("result")[0].innerHTML = sum
+
+        if (faces.length == 6) {
+            var details = []
+            solution.forEach((number, index) => { if (number) details.push(` ${number}x${index+1}`) })
+
+            section.getElementsByClassName("result-details")[0].innerHTML = details.join(",")
+        }
     }))
 })
