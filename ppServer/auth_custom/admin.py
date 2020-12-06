411b59439563b3b45e8297659a8dcd68fc6dc504
vsteinb <vsteinbruegg@uos.de>
2020-12-06 16:14:14 +0100

    more informative /admin/auth/user

--- ppServer/auth_custom/admin.py
+++ ppServer/auth_custom/admin.py
@@ -4,50 +4,13 @@ from django.contrib.auth.models import User, Group
 
 
 # Define a new User admin
-class UserAdmin(UserAdmin):
-    def has_add_permission(self, request):
-        if request.user.groups.filter(name__iexact="shop angucken").exists():
-            return False
-        else:
-            return super().has_add_permission(request)
+class CustomUserAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("is_superuser", "groups_",)
 
-    def has_delete_permission(self, request, obj=None):
-        if request.user.groups.filter(name__iexact="shop angucken").exists():
-            return False
-        else:
-            return super().has_delete_permission(request, obj)
-
-    def has_change_permission(self, request, obj=None):
-        if request.user.groups.filter(name__iexact="shop angucken").exists():
-            return False
-        else:
-            return super().has_change_permission(request, obj)
-
-
-class GroupAdmin(GroupAdmin):
-
-    def has_add_permission(self, request):
-        if request.user.groups.filter(name__iexact="shop angucken").exists():
-            return False
-        else:
-            return super().has_add_permission(request)
-
-    def has_delete_permission(self, request, obj=None):
-        if request.user.groups.filter(name__iexact="shop angucken").exists():
-            return False
-        else:
-            return super().has_delete_permission(request, obj)
-
-    def has_change_permission(self, request, obj=None):
-        if request.user.groups.filter(name__iexact="shop angucken").exists():
-            return False
-        else:
-            return super().has_change_permission(request, obj)
-
+    def groups_(self, obj):
+        return ", ".join([g.name for g in obj.groups.all()])
+    admin.boolean = True
 
 # Re-register UserAdmin
 admin.site.unregister(User)
-admin.site.register(User, UserAdmin)
-
-admin.site.unregister(Group)
-admin.site.register(Group, GroupAdmin)
+admin.site.register(User, CustomUserAdmin)
