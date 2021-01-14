41d54074f539a5e31f038d821036d5c47eadb322
Vanessa Steinbrügge <v.steinbr@gmail.com>
2021-01-14 21:13:47 +0100

    added automatic backup

--- ppServer/scripts/dump_db.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-x=1
-y=$(date +"%w")
-
-if (( x == y )); then
-    datum=$(date +%y-%m-%d)
-    cp /home/vstein/penPaperWeb/ppServer/db.sqlite3 /home/vstein/penPaperWeb/ppServer/dumps/$datum
-fi
