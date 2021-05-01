93e18b37b71909ae40b16b7b4d860c155b564e5d
Vanessa Steinbrügge <v.steinbr@gmail.com>
2021-05-01 17:40:38 +0200

    lighting

--- ppServer/mining/static/mining/js/mining.js
+++ ppServer/mining/static/mining/js/mining.js
@@ -3,6 +3,8 @@ let max_pos;
 
 let pressed_keys = [];
 
+let collision_map;
+
 document.addEventListener("DOMContentLoaded", () => {
     char = {tag: document.querySelector("#char"), pos: {x: 0, y: 0}};
     max_pos = {
@@ -10,9 +12,13 @@ document.addEventListener("DOMContentLoaded", () => {
         y: parseInt(document.querySelector("#height").innerHTML) - 1
     }
 
-    document.addEventListener("mousemove", e => {
-        const cell = e.target.closest(".cell");
-    });
+    // document.addEventListener("mousemove", e => {
+    //     const cell = e.target.closest(".cell");
+    // });
+
+    // fill collision map
+    init_collision_map();
+    apply_lighting(1, 2, 1);
 
     // setup keys to fire once on press
     document.addEventListener("keyup", e => {
@@ -40,12 +46,28 @@ document.addEventListener("DOMContentLoaded", () => {
     document.querySelector(".field-container--char").addEventListener('transitionend', e => {
         if (["top", "left"].includes(e.propertyName)) {
             document.documentElement.style.setProperty("--char-img", "var(--char-img-front)");
+            move_char_to(char.pos.x, char.pos.y + 1);
         }
       });
 
     prepopulate_field();
     spawn_char();
 });
+
+function create_empty_map(value) {
+    return Array.from({length: max_pos.y + 1}, () => Array.from({length: max_pos.x + 1}, () => { return JSON.parse(JSON.stringify(value)); }));
+}
+
+function init_collision_map() {
+    collision_map = create_empty_map({});
+    [...document.querySelectorAll(".field-container[data-collidable]")]
+        .reduce((acc, layer) => {
+            layer.querySelectorAll(".cell[data-material_id]").forEach(cell => {
+                acc[cell.dataset.y_pos][cell.dataset.x_pos][layer.dataset.layer_index] = cell;
+            });
+            return acc;
+        }, collision_map);
+}
 
 function prepopulate_field() {
     const prepopulated_cells = [...document.querySelectorAll(".cell")].filter(cell => cell.hasAttribute("data-material_id"));
@@ -60,6 +82,72 @@ function prepopulate_field() {
         cell.style.backgroundImage = url ? `url(${url})` : "";
     });
 }
+
+function apply_lighting(max_brightness, factor, max_depth) {
+
+    // get list of collidable block positions for lighting
+    const collision_pos = collision_map
+        .reduce((acc, row) => [...(acc || []), ...row], [])
+        .filter(cell => Object.keys(cell).length && Object.values(cell).length)
+        .map(cell => {
+            tag = Object.values(cell)[0];
+            return {x: parseInt(tag.dataset.x_pos), y: parseInt(tag.dataset.y_pos)};
+        });
+
+    // compute lighting
+    const lighting_map = apply_lighting_of(max_brightness, factor, max_depth, collision_pos);
+
+    // apply lighting to those blocks
+    lighting_map.forEach((row, y) => row.forEach((brightness, x) => {
+        Object.values(collision_map[y][x]).forEach(cell_layer => cell_layer.style.filter = `brightness(${brightness})`)
+    }));
+}
+
+function apply_lighting_of(brightness, factor, max_depth, collision_pos) {
+    return _apply_lighting_of(brightness, factor, max_depth, collision_pos);
+}
+
+// internal use only! Don't call directly, use apply_lighting_of() instead!
+function _apply_lighting_of(brightness, factor, max_depth, collision_pos, _lighting_map) {
+    if (max_depth <= 0) { return _lighting_map; }
+
+    // init map with brightness * 2 wherever air is 
+    if (!_lighting_map) {
+        _lighting_map = create_empty_map(brightness * factor);
+        collision_pos.forEach(cell => _lighting_map[cell.y][cell.x] = 0);
+        return _apply_lighting_of(brightness, factor, max_depth, collision_pos, _lighting_map);
+    }
+
+    // apply brightness if none set jet AND a neighboring cell was brighter
+    collision_pos.forEach(cell => {
+
+        const neighbors = get_neighbor_pos(cell.x, cell.y);
+        if (!_lighting_map[cell.y][cell.x] && neighbors.some(pos => _lighting_map[pos.y][pos.x] === brightness * factor)) {
+            _lighting_map[cell.y][cell.x] = brightness;
+        }
+    });
+
+    return _apply_lighting_of(brightness / factor, factor, max_depth - 1, collision_pos, _lighting_map);
+}
+
+function get_neighbor_pos(x, y) {
+    neighbors = [];
+    for (let _x = x - 1; _x <= x + 1; _x++) {
+        for (let _y = y - 1; _y <= y + 1; _y++) {
+            neighbors.push({x: _x, y: _y});
+        }
+    }
+
+    return neighbors.filter(pos => {
+        if (pos.x === x && pos.y === y) { return false; }
+
+        // in bounds of field?
+        if (pos.x < 0 || pos.y < 0) { return false; }
+        if (pos.x > max_pos.x || pos.y > max_pos.y) { return false; }
+        return true;
+    })
+}
+
 
 const field_size = 64;
 function convert_to_char_game_coords(field_coord, max_field_coord, max_game_coord) {
@@ -79,9 +167,19 @@ function convert_to_char_game_coords(field_coord, max_field_coord, max_game_coor
 }
 
 function move_char_to(field_x, field_y) {
+    
     // stay in FIELD coordinates, not out of bounds
     field_x = Math.max(0, Math.min(field_x, max_pos.x));
     field_y = Math.max(0, Math.min(field_y, max_pos.y));
+
+    // collision detection
+    if (Object.keys(collision_map[field_y][field_x]).length) {
+        // if moved in x direction: try to also move up
+        if (field_x !== char.pos.x && field_y === char.pos.y) {
+            move_char_to(field_x, char.pos.y - 1);
+        }
+        return;
+    }
     
     // TODO can be done in main
     const game_height = document.querySelector(".field").offsetHeight;
