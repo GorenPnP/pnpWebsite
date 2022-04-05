a32984274306e954f61f1ae445733cdfa1df6352
V Steinbrügge <v.steinbr@gmail.com>
2022-04-05 18:46:16 +0200

    fix spinner

--- ppServer/base/static/base/js/main.js
+++ ppServer/base/static/base/js/main.js
@@ -1,8 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
-	//setTimeout(() => document.getElementsByClassName("spinner-container")[0].style.display = 'none', 500)
-
 	window.onbeforeunload = function () {
 		document.getElementsByClassName("spinner-container")[0].style.display = 'flex'
 		document.getElementsByClassName("spinner-container")[0].style.opacity = 1;
 	};
 })
+
+document.onreadystatechange = function () {
+    if (this.readyState === "interactive") {
+		document.getElementsByClassName("spinner-container")[0].style.display = 'none'
+	}
+}
