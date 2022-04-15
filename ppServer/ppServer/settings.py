21ec1de20809fefa9d3c9c440e14fbe3ae282939
ovh server <>
2022-04-15 22:10:44 +0200

    noreply@goren-pnp.de

--- ppServer/ppServer/settings.py
+++ ppServer/ppServer/settings.py
@@ -186,6 +186,7 @@ EMAIL_PORT = os.environ.get('EMAIL_PORT')
 EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS')
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL')
 
 # show debug-toolbar if matches
 INTERNAL_IPS = ["localhost", "127.0.0.1"] if DEBUG else []
