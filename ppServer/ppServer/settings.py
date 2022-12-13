919997b88a0bbde73cb2119f7770541aa55e0a3c
V Steinbrügge <v.steinbr@gmail.com>
2022-12-13 17:31:49 +0100

    comment deps

--- ppServer/ppServer/settings.py
+++ ppServer/ppServer/settings.py
@@ -16,8 +16,6 @@ from datetime import timedelta
 import sentry_sdk
 from sentry_sdk.integrations.django import DjangoIntegration
 
-# TODO is temporary
-USE_DEPRECATED_PYTZ = True 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
