0cd6515f04dde2464c1ef6791b47602abf804cd2
V Steinbrügge <v.steinbr@gmail.com>
2022-12-13 16:30:05 +0100

    restrict spinner

--- ppServer/base/static/base/js/main.js
+++ ppServer/base/static/base/js/main.js
@@ -1,5 +1,9 @@
+let ready = false;
+
 document.addEventListener("DOMContentLoaded", function () {
 	window.onbeforeunload = function () {
+		if (ready) { return; }
+
 		document.getElementsByClassName("spinner-container")[0].style.display = 'flex'
 		document.getElementsByClassName("spinner-container")[0].style.opacity = 1;
 	};
@@ -7,6 +11,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
 document.onreadystatechange = function () {
     if (this.readyState === "interactive") {
+		ready = true;
 		document.getElementsByClassName("spinner-container")[0].style.display = 'none'
 	}
 }
