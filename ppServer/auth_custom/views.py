a29708b21163194d6cfc948c13f0a2eaa9df7470
V Steinbrügge <v.steinbr@gmail.com>
2023-11-24 11:50:37 +0100

    skip registration email send if email address already exists

--- ppServer/auth_custom/views.py
+++ ppServer/auth_custom/views.py
@@ -32,6 +32,9 @@ def signup(request):
         if form.is_valid():
             user = form.save(commit=False)
 
+            if User.objects.filter(email = user.email).exists():
+                return redirect('auth:signup_done')
+
             # give user spieler permissions
             user.is_active = False
             user.is_staff = True
