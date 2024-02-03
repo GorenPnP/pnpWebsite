060bc654261c8fe74e70a84698dfb15784dc882e
V Steinbrügge <v.steinbr@gmail.com>
2024-02-03 13:18:56 +0100

    wiki: restructure Rule-text with markdown, less models

--- ppServer/wiki/admin.py
+++ ppServer/wiki/admin.py
@@ -1,36 +1,37 @@
 from django.contrib import admin
 from django.http.request import HttpRequest
-from django.utils.html import format_html
+
+from markdownfield.widgets import MDEAdminWidget
 
 from .models import *
 
 class RuleAdmin(admin.ModelAdmin):
-    list_display = ["nr", "titel", "_text"]
+    class Media:
+        css = {
+            "all": ["wiki/css/md_editor.css"],
+        }
+
+    list_display = ["nr", "titel"]
     list_editable = ["nr"]
     list_display_links = ["titel"]
 
-    def _text(self, obj):
-        return format_html(obj.text_rendered)
+    # use md-editor for a(ll) TextFields
+    formfield_overrides = {
+        models.TextField: {"widget": MDEAdminWidget(options={
+        "spellChecker": False,
+        "toolbar": [
+            "undo", "redo", "|",
+            "bold", "italic", "heading-1", "heading-2", "heading-3", "|",
+            "unordered-list", "ordered-list", "table", "|",
+            "link", "quote", "|",
+            "guide"
+        ]
+        })},
+    }
 
     
     def has_module_permission(self, request: HttpRequest) -> bool:
         return request.user.groups.filter(name__iexact="spielleiter").exists()
 
-
-class GhostAdmin(admin.ModelAdmin):
-    list_display = ["titel"]
-    
-    def has_module_permission(self, request: HttpRequest) -> bool:
-        return request.user.groups.filter(name__iexact="spielleiter").exists()
-
-
-class RuleTableAdmin(admin.ModelAdmin):
-    list_display = ["topic"]
-    readonly_fields = ["topic"]
-    
-    def has_module_permission(self, request: HttpRequest) -> bool:
-        return request.user.groups.filter(name__iexact="spielleiter").exists()
 
 admin.site.register(Rule, RuleAdmin)
-admin.site.register(Ghost, GhostAdmin)
-admin.site.register(RuleTable, RuleTableAdmin)
--- ppServer/wiki/models.py
+++ ppServer/wiki/models.py
@@ -1,8 +1,5 @@
 from django.db import models
 
-from markdownfield.models import MarkdownField, RenderedMarkdownField
-from markdownfield.validators import VALIDATOR_STANDARD
-
 
 class Rule(models.Model):
 
@@ -13,46 +10,7 @@ class Rule(models.Model):
 
     nr = models.PositiveIntegerField(default=0)
     titel = models.CharField(max_length=256, unique=True, null=False)
-
-    text = MarkdownField(rendered_field='text_rendered', validator=VALIDATOR_STANDARD)
-    text_rendered = RenderedMarkdownField(null=False)
+    text = models.TextField(default="", null=False, blank=False)    # markdown-text
 
     def __str__(self):
         return "{}: {}".format(self.nr, self.titel)
-    
-class Ghost(models.Model):
-
-    class Meta:
-        ordering = ['titel']
-        verbose_name = "Geist"
-        verbose_name_plural = "Geister"
-
-    titel = models.CharField(max_length=64, unique=True)
-    sch = models.CharField(max_length=64, default="", verbose_name="Schnelligkeit")
-    angriff = models.CharField(max_length=64, default="", verbose_name="Angriff")
-    ma = models.CharField(max_length=64, default="", verbose_name="Magie")
-    wk = models.CharField(max_length=64, default="", verbose_name="Willenskraft")
-    st = models.CharField(max_length=64, default="", verbose_name="Stärke")
-    schaWI = models.CharField(max_length=64, default="", verbose_name="Schadenswiderstand")
-    reaktion = models.CharField(max_length=64, default="", verbose_name="Reaktion")
-    schaden_pro_erfolg = models.CharField(max_length=64, default="", verbose_name="Schaden / Erfolg")
-    initiative = models.CharField(max_length=64, default="", verbose_name="Initiative")
-    astralschaden = models.CharField(max_length=64, default="", verbose_name="Astralschaden")
-
-    eigenschaft = models.TextField(default="")
-
-    def __str__(self):
-        return self.titel
-    
-class RuleTable(models.Model):
-
-    class Meta:
-        ordering = ['topic']
-        verbose_name = "Regel-Tabelle"
-        verbose_name_plural = "Regel-Tabellen"
-
-    topic = models.CharField(max_length=128, unique=True)
-    csv_data = models.TextField(default="")
-
-    def __str__(self):
-        return self.topic
