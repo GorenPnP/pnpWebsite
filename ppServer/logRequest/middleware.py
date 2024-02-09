9b9385e6a434e1d8455e76332b67ab9922ffd084
V Steinbrügge <v.steinbr@gmail.com>
2024-02-09 16:03:29 +0100

    logging requests: exclude my uptimeKuma monitoring

--- ppServer/logRequest/middleware.py
+++ ppServer/logRequest/middleware.py
@@ -10,15 +10,18 @@ class RequestMiddleware:
         self.get_response = get_response
         self.path_blacklist = ("/static", "/media", "/admin", "/api")
         self.favicon_filename = "favicon.ico"
+        self.ip_blacklist = ("139.162.134.44",)    # my Uptime_Kuma-monitoring
         # One-time configuration and initialization.
 
     def __call__(self, request):
         # Code to be executed for each request before
         # the view (and later middleware) are called.
 
+        ip = (request.META["HTTP_X_FORWARDED_FOR"] if "HTTP_X_FORWARDED_FOR" in request.META else "someone?")
         if DEBUG or\
             request.scope["path"].startswith(self.path_blacklist) or\
             self.favicon_filename in request.scope["path"] or\
+            ip in self.ip_blacklist or\
             request.user.groups.filter(name__iexact="spielleiter").exists():
 
             return self.get_response(request)
@@ -32,7 +35,7 @@ class RequestMiddleware:
             pfad=self.cap_string(request.scope["path"], 500),
             antwort=getattr(response, 'status_code', None),
             methode=request.scope["method"],
-            user=self.cap_string(request.user.username or (request.META["HTTP_X_FORWARDED_FOR"] if "HTTP_X_FORWARDED_FOR" in request.META else "someone?"), 200),
+            user=self.cap_string(request.user.username or ip, 200),
             user_agent=self.cap_string(user_agent, 200)
         )
 
