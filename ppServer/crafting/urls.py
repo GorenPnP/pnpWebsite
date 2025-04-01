adebbe1d4662508650d738593bd586a1ebfac0cd
V Steinbrügge <v.steinbr@gmail.com>
2025-04-01 23:06:39 +0200

    crafting: restrict mining to spielleiter (temporarily)

--- ppServer/crafting/urls.py
+++ ppServer/crafting/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 
+from ppServer.decorators import spielleiter_only
+
 from . import views
 from .api_views import GetMinecraftRecipesView
 
@@ -11,8 +13,8 @@ urlpatterns = [
 	path('craft/', views.CraftingView.as_view(), name='craft'),
 	path('details/<int:pk>/', views.RecipeDetailsView.as_view(), name='details'),
 	path('sp_give_items/', views.SpGiveItemsView.as_view(), name='sp_give_items'),
-	path('regions/', views.RegionListView.as_view(), name='regions'),
-	path('mining/<int:pk>/', views.MiningView.as_view(), name='mining'),
+	path('regions/', spielleiter_only('crafting:inventory')(views.RegionListView.as_view()), name='regions'),
+	path('mining/<int:pk>/', spielleiter_only('crafting:inventory')(views.MiningView.as_view()), name='mining'),
 
 	path('api/mc_recipes/', GetMinecraftRecipesView.as_view(), name='api_mc_recipes'),
 ]
