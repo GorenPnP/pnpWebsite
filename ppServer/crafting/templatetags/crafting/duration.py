ee242d195c21e91a8f3a7d439a57857438620ae8
V Steinbrügge <v.steinbr@gmail.com>
2025-09-11 14:52:50 +0200

    crafting: format durations & rm mining->wechseln-btn

--- ppServer/crafting/templatetags/crafting/duration.py
+++ ppServer/crafting/templatetags/crafting/duration.py
@@ -1,11 +1,11 @@
 from datetime import timedelta
 
 from django import template
-from django.utils.duration import duration_string
+from django.utils.duration import _get_duration_components
 
 register = template.Library()
 
 @register.filter
 def duration(td: timedelta):
-    return duration_string(td) + " [dd ]hh:mm:ss[.6microseconds]"
-
+    days, hours, minutes, seconds, microseconds = _get_duration_components(td)
+    return f"{days} Tage {hours:02d} h {minutes:02d} min" if days else f"{hours:02d} h {minutes:02d} min"
