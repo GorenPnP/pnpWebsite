16dd0d8137242ede0fa6799dde97d45a7ce34450
V Steinbrügge <v.steinbr@gmail.com>
2025-12-13 18:22:10 +0100

    replace todAb with Totentokens

--- ppServer/characterExport/export.py
+++ ppServer/characterExport/export.py
@@ -506,8 +506,6 @@ class CharakterExporter:
         self._POSITION["kHP"] = f"Werte!J{ROW}"
         werte_ws.write(f"I{ROW}", "Körperliche HP", format_section_titel)
         werte_ws.write(f"J{ROW}", f"=J{ROW-2}+J{ROW-1}+({0 if self.char.larp else self._position('ep_stufe')}*2)+({self._position('ST')}*5)", format_hp)
-        werte_ws.write(f"K{ROW}", "Tot ab", format_section_titel)
-        werte_ws.write(f"L{ROW}", f"=(-2)*{self._position('ST')}", format_hp)
         
         ROW += 1
         werte_ws.write(f"I{ROW}", "G HP Bonus", format_align_center_center)
--- ppServer/polls/admin.py
+++ ppServer/polls/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-from django.utils.timezone import now
 
 from .models import Question, Choice, QuestionSpieler
 
