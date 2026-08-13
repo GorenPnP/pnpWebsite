ffaa8e9c49f21731ea820b9653f3c56fd9747a14
V Steinbrügge <v.steinbr@gmail.com>
2026-08-13 18:13:29 +0200

    Sentry PNP-SERVER-F5: make card on char on char deletion optional to not throw an error if it does not exist

--- ppServer/backend/cards/signals.py
+++ ppServer/backend/cards/signals.py
@@ -11,14 +11,13 @@ from .models import Card
 # Solution: trigger deactivation manually and delete Transactions afterwards to force the correct order of things.
 @receiver(pre_delete, sender=Charakter)
 def deactivate_releffects_before_deleting_card_of_char(sender, instance, **kwargs):
-    for releffect in instance.releffect_set.all(): releffect.deactivate(False)
-    if instance.card: delete_transactions_when_deleting_card(Card, instance.card)
+    if instance.card:
+        for releffect in instance.releffect_set.filter(is_active=True): releffect.deactivate(False)
+        delete_transactions_when_deleting_card(Card, instance.card)
 
 
 @receiver(pre_delete, sender=Card)
 def delete_transactions_when_deleting_card(sender, instance, **kwargs):
     ''' delete transactions that will be sender = receiver = None, therefore will get invalid '''
-    if instance.card_distributed_to_player:
-        raise ValueError(f"{instance._meta.object_name} kann nicht gelöscht werden. Sammle die physische Karte erst wieder vom Spieler ein.")
 
     instance.get_transactions().filter(Q(sender=None) | Q(receiver=None)).delete()
